688925101356740b830e1106fd8c2470b256157b
wyne-ybanez <wyne_01@icloud.com>
2021-08-05 23:59:24 +0000

    Change color and font weight for nav menu, add products loop to horizontal scroll

--- home/views.py
+++ home/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from products.models import Product
 
 
 def index(request):
     """ 
-    View to return the index page
+    View to return the index page.
+    Display Products
     """
-    return render(request, 'home/index.html')
+
+    products = Product.objects.all()
+    context = {
+        'products': products
+    }
+
+    return render(request, 'home/index.html', context)
