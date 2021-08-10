1d364186a5bd924059ac4ef57da85139042ea2c3
wyne-ybanez <wyne_01@icloud.com>
2021-08-10 13:15:52 +0000

    Fix horizontal scroll card padding

--- products/views.py
+++ products/views.py
@@ -21,10 +21,8 @@ def all_products(request):
                 sortkey = 'lower_name'
                 # Annotate into field
                 products = products.annotate(lower_name=Lower('name'))
-
             if sortkey == 'category':
                 sortkey = 'category__name'
-
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
