9198221c6781ae0f63b33c4086a01a7cf351c255
wyne-ybanez <wyne_01@icloud.com>
2021-08-15 15:33:18 +0000

    Add add to bag functionality and update product details page set up

--- basket/views.py
+++ basket/views.py
@@ -1,8 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def view_basket(request):
     """
-    A view to return the shopping basket
+    A view to return the shopping basket.
     """
     return render(request, 'basket/basket.html')
+
+
+def add_to_basket(request, item_id):
+    """
+    Add a quanitity of the desired product and place it in basket.
+    Stores shopping basket data in a 'session'.
+    Get 'basket' in session or create basket object if it doesn't exist.
+    """
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    basket = request.session.get('basket', {})
+
+    if item_id in list(basket.keys()):
+        basket[item_id] += quantity
+    else:
+        basket[item_id] = quantity
+    
+    request.session['basket'] = basket
+    print(request.session['basket'])
+    return redirect(redirect_url)
