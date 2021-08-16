bcd2bdc33c7cff912f29b0e657dc2a52206cfc7c
wyne-ybanez <wyne_01@icloud.com>
2021-08-16 13:29:43 +0000

    Update context processor to display basket items total and quantities

--- basket/context.py
+++ basket/context.py
@@ -1,20 +1,33 @@
 from decimal import Decimal
 from django.conf import settings
 from django.shortcuts import get_object_or_404
+from products.models import Product
 
 
 def basket_contents(request):
     """
+    Shows the contents within basket.
+
     Placed in settings.py, made available to all templates.
     Display delivery threshold.
+
     FREE_DELIVERY_THRESHOLD = 100
     STANDARD_DELIVERY_PERCENTAGE = 10
     """
-
     basket_items = []
     total = 0
     product_count = 0
     basket = request.session.get('basket', {})
+
+    for item_id, quantity in basket.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity 
+        basket_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
@@ -33,6 +46,7 @@ def basket_contents(request):
         'free_delivery_delta': free_delivery_delta,
         'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
         'grand_total': grand_total,
+        'basket': basket,
     }
 
     return context
