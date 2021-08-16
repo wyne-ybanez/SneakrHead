558f0fb6b8a0f92f0aea2b439b1814b4bac06530
wyne-ybanez <wyne_01@icloud.com>
2021-08-16 17:28:39 +0000

    Added sizes to product model and size selector box to product details template

--- products/models.py
+++ products/models.py
@@ -19,14 +19,15 @@ class Product(models.Model):
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
+    has_sizes = models.BooleanField(default=False, null=False, blank=True)
     stock = models.IntegerField(default=0)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True, default='media/noimage.png',)
     featured_product = models.BooleanField(default=False)
-    new_product = models.BooleanField(default=False)
-    brand_item = models.BooleanField(default=False)
+    new_product = models.BooleanField(default=False, null=False, blank=True)
+    brand_item = models.BooleanField(default=False, null=False, blank=True)
 
     def __str__(self):
         return self.name
