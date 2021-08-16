fb55af6e099e73ad02f869ab490e9fc0cf2a359c
wyne-ybanez <wyne_01@icloud.com>
2021-08-16 18:46:56 +0000

    Implement product size logic, separate categories display into includes folder, remove add to bag ability if product out of stock

--- basket/context.py
+++ basket/context.py
@@ -7,6 +7,7 @@ from products.models import Product
 def basket_contents(request):
     """
     Shows the contents within basket.
+    Displays quantities and checks for sizes.
 
     Placed in settings.py, made available to all templates.
     Display delivery threshold.
@@ -19,15 +20,29 @@ def basket_contents(request):
     product_count = 0
     basket = request.session.get('basket', {})
 
-    for item_id, quantity in basket.items():
-        product = get_object_or_404(Product, pk=item_id)
-        total += quantity * product.price
-        product_count += quantity 
-        basket_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'product': product,
-        })
+    for item_id, item_data in basket.items():
+        # Check if item has no size.
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.price
+            product_count += item_data 
+            basket_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+            })
+        else: 
+            # Else there is a size, data is kept as a dictionary, iterate through dictionary.
+            product = get_object_or_404(Product, pk=item_id)
+            for size, quantity in item_data['item_by_size'].items():
+                total += quantity * product.price
+                product_count += quantity
+                basket_items.append({
+                    'item_id': item_id,
+                    'quantity': item_data,
+                    'product': product,
+                    'size': size,
+                })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
--- basket/views.py
+++ basket/views.py
@@ -10,18 +10,30 @@ def view_basket(request):
 
 def add_to_basket(request, item_id):
     """
-    Add a quanitity of the desired product and place it in basket.
+    Add a quantity of the desired product, its sizes and place it in basket.
     Stores shopping basket data in a 'session'.
     Get 'basket' in session or create basket object if it doesn't exist.
     """
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
     basket = request.session.get('basket', {})
 
-    if item_id in list(basket.keys()):
-        basket[item_id] += quantity
+    if size: 
+        if item_id in list(basket.keys()):
+            if size in basket[item_id]['item_by_size'].keys():
+                basket[item_id]['item_by_size'][size] += quantity
+            else: 
+                basket[item_id]['item_by_size'][size] = quantity
+        else: 
+            basket[item_id] = {'item_by_size': {size: quantity}}
     else:
-        basket[item_id] = quantity
+        if item_id in list(basket.keys()):
+            basket[item_id] += quantity
+        else:
+            basket[item_id] = quantity
     
     request.session['basket'] = basket
     return redirect(redirect_url)
