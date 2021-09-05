e0ff5247af7b73a24384e687249032200d36b794
wyne-ybanez <wyne_01@icloud.com>
2021-09-05 18:29:14 +0000

    Revert "Add new product model and start deployment to heroku"
    This reverts commit 03f128cf0f918e06c5586dd7a1cd14955b79178c.

--- home/models.py
+++ home/models.py
@@ -1,9 +1,3 @@
 from django.db import models
 
-
-# class Newsletter(models.Model):
-#     subscribe_email = models.EmailField(max_length=100)
-#     subscribed = models.BooleanField(default=True)
-
-#     def __str__(self):
-#         return self.subscribe_email
+# Create your models here.
--- products/models.py
+++ products/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-from profiles.models import UserProfile
-
 
 class Category(models.Model):   
     class Meta:
@@ -33,13 +31,3 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
-
-class Brand(models.Model):
-    name = models.CharField(max_length=50)
-    friendly_name = models.CharField(max_length=50, null=True, blank=True)
-
-    def __str__(self):
-        return self.name
-
-    def get_friendly_name(self):
-        return self.friendly_name
