38064578e076e8cf8d63c32ba4fee83970e4230c
wyne-ybanez <wyne_01@icloud.com>
2021-09-05 21:17:52 +0000

    Refine settings.py database logic, push database changes to git and heroku, add product brand

--- sneakrheads/settings.py
+++ sneakrheads/settings.py
@@ -124,17 +124,18 @@ WSGI_APPLICATION = 'sneakrheads.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if 'DATABASE_URL' in os.environ:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL')),
-    }
-else:
+if 'DEVELOPMENT' in os.environ:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
+else:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL')),
+    }
+    
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
