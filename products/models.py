03f128cf0f918e06c5586dd7a1cd14955b79178c
wyne-ybanez <wyne_01@icloud.com>
2021-09-05 18:23:04 +0000

    Add new product model and start deployment to heroku

--- products/models.py
+++ products/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from profiles.models import UserProfile
+
 
 class Category(models.Model):   
     class Meta:
@@ -31,3 +33,13 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+class Brand(models.Model):
+    name = models.CharField(max_length=50)
+    friendly_name = models.CharField(max_length=50, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
--- sneakrheads/settings.py
+++ sneakrheads/settings.py
@@ -124,16 +124,16 @@ WSGI_APPLICATION = 'sneakrheads.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 # if 'DATABASE_URL' in os.environ:
-#     DATABASES = {
-#         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-#     }
-# else:
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse('postgres://djnjayxdkxvkyw:2268062c5ca0cb7a0052848148e4f84e1c6081b1f07d26b288f575277d0400c9@ec2-54-220-170-192.eu-west-1.compute.amazonaws.com:5432/daqis1ailvluc9')
 }
+# else:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
