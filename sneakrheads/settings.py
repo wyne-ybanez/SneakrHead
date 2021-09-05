e6db295a5faa43fa288beea8ee46610cc7694653
wyne-ybanez <wyne_01@icloud.com>
2021-09-05 21:28:04 +0000

    Deploy to heroku bug fix, changes made to database settings

--- sneakrheads/settings.py
+++ sneakrheads/settings.py
@@ -126,15 +126,16 @@ WSGI_APPLICATION = 'sneakrheads.wsgi.application'
 
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL')),
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
