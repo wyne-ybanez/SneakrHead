a56dbea0eeb372bb6f65ff785e583095d73562a0
wyne-ybanez <wyne_01@icloud.com>
2021-09-05 20:20:29 +0000

    Add cache control

--- profiles/models.py
+++ profiles/models.py
@@ -29,10 +29,7 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
     """
     Create or update the user profile
     """
-    # try:
     if created:
         UserProfile.objects.create(user=instance)
-        # Existing users: just save the profile
+    # Existing users: just save the profile
     instance.userprofile.save()
-    # except ObjectDoesNotExist:
-    #     UserProfile.objects.create(user=instance)
