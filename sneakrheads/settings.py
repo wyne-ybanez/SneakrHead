794a914268a61b887f0b054aa40d096aa5b5f6e2
wyne-ybanez <wyne_01@icloud.com>
2021-09-05 23:10:43 +0000

    Reset heroku database, upload data again to heroku for deployment

--- sneakrheads/settings.py
+++ sneakrheads/settings.py
@@ -25,8 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['wyne-sneakrhead.herokuapp.com', 'localhost']
 
@@ -136,7 +135,6 @@ else:
             'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
-
 
 
 # Password validation
