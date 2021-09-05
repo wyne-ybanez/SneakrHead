ec5746d95e2fa8bb8746f67e89df06d1f1be0c1b
wyne-ybanez <wyne_01@icloud.com>
2021-09-05 21:30:56 +0000

    Revert database addition for brand to fix bug for heroku error 500 bug

--- products/models.py
+++ products/models.py
@@ -16,7 +16,7 @@ class Category(models.Model):
 
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
-    brand = models.ForeignKey('Brand', null=True, blank=True, on_delete=models.SET_NULL)
+    # brand = models.ForeignKey('Brand', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
@@ -33,12 +33,12 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
-class Brand(models.Model):
-    name = models.CharField(max_length=50)
-    friendly_name = models.CharField(max_length=50, null=True, blank=True)
+# class Brand(models.Model):
+#     name = models.CharField(max_length=50)
+#     friendly_name = models.CharField(max_length=50, null=True, blank=True)
 
-    def __str__(self):
-        return self.name
+#     def __str__(self):
+#         return self.name
 
-    def get_friendly_name(self):
-        return self.friendly_name
+#     def get_friendly_name(self):
+#         return self.friendly_name
