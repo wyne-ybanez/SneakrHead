aa7fca2e381f5c07fc32bb8824614c9c5489678b
wyne-ybanez <wyne_01@icloud.com>
2021-09-06 18:16:07 +0000

    Refactor nav menu for mobiles and site responsivity with add product form

--- products/forms.py
+++ products/forms.py
@@ -11,10 +11,10 @@ class ProductForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         categories = Category.objects.all()
-        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+        categories_friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
 
         brands = Brand.objects.all()
-        friendly_names = [(b.id, b.get_friendly_name()) for b in brands]
+        brand_friendly_names = [(b.id, b.get_friendly_name()) for b in brands]
 
-        self.fields['category'].choices = friendly_names
-        self.fields['brand'].choices = friendly_names
+        self.fields['category'].choices = categories_friendly_names
+        self.fields['brand'].choices = brand_friendly_names
